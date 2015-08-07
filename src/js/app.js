0f2aa4083a69a9f6bffe56f9ea507d4e0c840967
Naved Khan <navidkhn1@gmail.com>
2015-08-07 21:24:54 +0530

    Updates

--- src/js/app.js
+++
@@ -1,9 +0,0 @@
-m = require('mithril');
-var semanticUi = require('semantic');
-
-//this application only has one module: todo
-var app = {};
-
-
-//initialize the application
-m.module(document.getElementById('app'), app);
