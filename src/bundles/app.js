54ea84de2c50ee9905de1c698cc7a848979435b6
Hitesh <me@hiteshjoshi.com>
2015-09-09 03:31:34 +0530

    ux

--- src/bundles/app.js
+++ src/bundles/app.js
@@ -720,117 +720,83 @@ webpackJsonp([0],[
 
 	  .directive('tree', function($window) {
 	      return {
-	        restrict: 'A',
+
+	            restrict: 'A',
 	            replace: true,
 	            template: '<div class="chart" id="OrganiseChart6"></div>',
-	        link: function(scope, element, attrs, fn) {
+	            scope: {
+	                json : null
+	            },
+	            link: function(scope, element, attrs, fn) {
+
+	              var margin = {top: 0, right: 320, bottom: 0, left: 0},
+	                  width = 960 - margin.left - margin.right,
+	                  height = 500 - margin.top - margin.bottom;
+
+	              var tree = d3.layout.tree()
+	                  .separation(function(a, b) { return a.parent === b.parent ? 1 : .5; })
+	                  .children(function(d) { return d.parents; })
+	                  .size([height, width]);
+
+	              var svg = d3.select("#OrganiseChart6").append("svg")
+	                  .attr("width", width + margin.left + margin.right)
+	                  .attr("height", height + margin.top + margin.bottom)
+	                .append("g")
+	                  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 
-	          var margin = {top: 0, right: 320, bottom: 0, left: 0},
-	              width = 960 - margin.left - margin.right,
-	              height = 500 - margin.top - margin.bottom;
+	              
+	                var start_rendering = function(theValue){
+	                  var nodes = tree.nodes(theValue);
 
-	          var tree = d3.layout.tree()
-	              .separation(function(a, b) { return a.parent === b.parent ? 1 : .5; })
-	              .children(function(d) { return d.parents; })
-	              .size([height, width]);
+	                  var link = svg.selectAll(".link")
+	                      .data(tree.links(nodes))
+	                    .enter().append("path")
+	                      .attr("class", "link")
+	                      .attr("d", elbow);
 
-	          var svg = d3.select("#OrganiseChart6").append("svg")
-	              .attr("width", width + margin.left + margin.right)
-	              .attr("height", height + margin.top + margin.bottom)
-	            .append("g")
-	              .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+	                  var node = svg.selectAll(".node")
+	                      .data(nodes)
+	                    .enter().append("g")
+	                      .attr("class", "node")
+	                      .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; })
+
+	                  node.append("text")
+	                      .attr("class", "name")
+	                      .attr("x", 8)
+	                      .attr("y", -6)
+	                      .text(function(d) { return d.name; });
+
+	                  node.append("text")
+	                      .attr("x", 8)
+	                      .attr("y", 8)
+	                      .attr("dy", ".71em")
+	                      .attr("class", "about lifespan")
+	                      .text(function(d) { return d.born + "–" + d.died; });
+
+	                  node.append("text")
+	                      .attr("x", 8)
+	                      .attr("y", 8)
+	                      .attr("dy", "1.86em")
+	                      .attr("class", "about location")
+	                      .text(function(d) { return d.location; });
+	                }
+	                
 
 
-	          var json = {
-	            "name": "Clifford Shanks",
-	            "born": 1862,
-	            "died": 1906,
-	            "location": "Petersburg, VA",
-	            "parents": [
-	              {
-	                "name": "James Shanks",
-	                "born": 1831,
-	                "died": 1884,
-	                "location": "Petersburg, VA",
-	                "parents": [
-	                  {
-	                    "name": "Robert Shanks",
-	                    "born": 1781,
-	                    "died": 1871,
-	                    "location": "Ireland/Petersburg, VA"
-	                  },
-	                  {
-	                    "name": "Elizabeth Shanks",
-	                    "born": 1795,
-	                    "died": 1871,
-	                    "location": "Ireland/Petersburg, VA"
-	                  }
-	                ]
-	              },
-	              {
-	                "name": "Ann Emily Brown",
-	                "born": 1826,
-	                "died": 1866,
-	                "location": "Brunswick/Petersburg, VA",
-	                "parents": [
-	                  {
-	                    "name": "Henry Brown",
-	                    "born": 1792,
-	                    "died": 1845,
-	                    "location": "Montgomery, NC"
-	                  },
-	                  {
-	                    "name": "Sarah Houchins",
-	                    "born": 1793,
-	                    "died": 1882,
-	                    "location": "Montgomery, NC"
-	                  }
-	                ]
+	                scope.$watch('json', function(newValue, oldValue) {
+	                    if (newValue !== oldValue) {
+	                      // You actions here
+	                      start_rendering(newValue);
+	                      console.log("I got the new value! ", newValue);
+	                    }
+	                }, true);
+
+	              function elbow(d, i) {
+	                return "M" + d.source.y + "," + d.source.x
+	                     + "H" + d.target.y + "V" + d.target.x
+	                     + (d.target.children ? "" : "h" + margin.right);
 	              }
-	            ]
-	          }
-	            
-
-	            var nodes = tree.nodes(json);
-
-	            var link = svg.selectAll(".link")
-	                .data(tree.links(nodes))
-	              .enter().append("path")
-	                .attr("class", "link")
-	                .attr("d", elbow);
-
-	            var node = svg.selectAll(".node")
-	                .data(nodes)
-	              .enter().append("g")
-	                .attr("class", "node")
-	                .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; })
-
-	            node.append("text")
-	                .attr("class", "name")
-	                .attr("x", 8)
-	                .attr("y", -6)
-	                .text(function(d) { return d.name; });
-
-	            node.append("text")
-	                .attr("x", 8)
-	                .attr("y", 8)
-	                .attr("dy", ".71em")
-	                .attr("class", "about lifespan")
-	                .text(function(d) { return d.born + "–" + d.died; });
-
-	            node.append("text")
-	                .attr("x", 8)
-	                .attr("y", 8)
-	                .attr("dy", "1.86em")
-	                .attr("class", "about location")
-	                .text(function(d) { return d.location; });
-
-	          function elbow(d, i) {
-	            return "M" + d.source.y + "," + d.source.x
-	                 + "H" + d.target.y + "V" + d.target.x
-	                 + (d.target.children ? "" : "h" + margin.right);
-	          }
 
 	        }
 	      };
